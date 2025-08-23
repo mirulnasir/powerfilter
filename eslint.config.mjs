5387e1122d5e2c4ff8dc45199468bbca96dabe2e
mirulnasir <creative.fla@gmail.com>
2025-08-23 11:13:50 +0800

    feat: fix ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  { ignores: ["**/.next/**", "**/node_modules/**", "out/**", "dist/**"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
-
 export default eslintConfig;
